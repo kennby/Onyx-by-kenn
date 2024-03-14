732ec050fc34edc5d7c7bedfab2b9d5ca1d27252
kennby <153956421+kennby@users.noreply.github.com>
2024-03-14 16:55:16 -0500

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;kenn;;\nFN:kenn\nORG:kenn\nTITLE:\nitem1.TEL;waid=51902247905:51902247905\nitem1.X-ABLabel:kenn\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:kenn\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;kenn;;\nFN:kenn\nORG:kenn\nTITLE:\nitem1.TEL;waid=51907364682:51907364682\nitem1.X-ABLabel:kenn\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:kenn\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'kenn', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
